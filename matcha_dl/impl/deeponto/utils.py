6f5c70639f7c393c7d7af81a91febe0b2a599612
pedro <pgcotovio@gmail.com>
2024-04-26 18:23:23 +0200

    [add] trainers

--- matcha_dl/impl/deeponto/utils.py
+++ matcha_dl/impl/deeponto/utils.py
@@ -3,6 +3,8 @@
 from typing import Optional
 import pandas as pd
 
+from deeponto.align.mapping import EntityMapping
+
 
 def sort_dict_by_values(dic: dict, desc: bool = True, top_k: Optional[int] = None):
     """Return a sorted dict by values with top k reserved
@@ -18,3 +20,28 @@ def read_table(file_path: str):
     na_vals = pd.io.parsers.readers.STR_NA_VALUES.difference({"NULL", "null", "n/a"})
     
     return pd.read_csv(file_path, sep=sep, na_values=na_vals, keep_default_na=False)
+
+def fill_anchored_scores(ref_anchored_maps, pred_maps):
+    """Fill scores of the anchored reference mappings with the scores of the predicted mappings."""
+
+    pred_maps_tuples = EntityMapping.as_tuples(pred_maps, with_score=True)
+
+    pred_maps_dict = {}
+    for source, tgt, score in pred_maps_tuples:
+        if not source in pred_maps_dict:
+            pred_maps_dict[source] = {}
+        pred_maps_dict[source][tgt] = score
+
+    results = []
+    for src_ref_class, tgt_ref_class, tgt_cands in ref_anchored_maps:
+        tgt_cands = eval(tgt_cands)
+        scored_cands = []
+        for tgt_cand in tgt_cands:
+            try:    
+                scored_cands.append((tgt_cand, pred_maps_dict[src_ref_class][tgt_cand]))
+
+            except KeyError:
+                scored_cands.append((tgt_cand, 0.0))
+
+        results.append((src_ref_class, tgt_ref_class, scored_cands))
+    return results
---
+++ matcha_dl/impl/mlp_trainer.py
@@ -0,0 +1,101 @@
+
+import warnings
+from torch.utils.tensorboard import SummaryWriter
+import tqdm
+
+from torch.utils.data import DataLoader, TensorDataset
+import torch as th
+
+from matcha_dl.core.contracts.trainer import ITrainer, EntityMapping
+
+import numpy as np
+
+
+class MLPTrainer(ITrainer):
+
+    def train(self, epochs=50, batch_size=None, save_interval=5):
+
+        warnings.filterwarnings("ignore", category=UserWarning)
+
+        writer = SummaryWriter(self.logs_dir)
+
+        while self.epoch <= epochs:
+            self._model.train()
+            _iter = 1
+            
+            with tqdm(self._load_data(kind='train', batch_size=batch_size), unit="batch") as tepoch:
+                
+                for data, target in tepoch:
+                    tepoch.set_description(f"Epoch {self.epoch}")
+
+                    self._optimizer.zero_grad()
+                    logits = self._model(data)
+                    loss = self._loss(logits, target)
+                    writer.add_scalar("Loss/train", loss, _iter)
+
+                    loss.backward()
+                    self._optimizer.step()
+
+                    tepoch.set_postfix(loss=loss.item())
+
+                    _iter += 1
+
+            if self.epoch % save_interval == 0:
+                self.save_checkpoint()
+
+            self._epoch += 1
+
+        writer.flush()
+        writer.close()
+
+    def filter(self, **kwargs):
+
+        # TODO add AML repair
+        pass
+
+    def predict(self, **kwargs):
+
+        kind = "inference"
+        
+        df = self.dataset.dataframe.copy()
+        df = df[df[kind] == True]
+
+        # if supervised use model to calculate scores
+        if self.dataset.reference:
+
+            data, _ = self._load_data(kind=kind)
+            self._model.eval()
+            with th.no_grad():
+                logits = self._model(data)
+
+            df['Scores'] = logits.cpu()
+
+            return [EntityMapping(dp["SrcEntity"], dp["TgtEntity"], "=", dp["Scores"]) for _, dp in df.iterrows()]
+        
+        # if unsupervised use max score from matcha
+        else:
+
+            df['matcha'] = np.array(df['Features'].values.tolist()).max(axis=1)
+
+            return [EntityMapping(dp["SrcEntity"], dp["TgtEntity"], "=", dp["matcha"]) for _, dp in df.iterrows()]
+
+    def _load_data(self, kind='train', batch_size=None):
+
+        x = self.dataset.x(kind)
+        y = self.dataset.y(kind)
+
+        x = th.tensor(x, dtype=th.float32)
+        x = x.to(self.device)
+
+        y = th.tensor(y, dtype=th.float32)
+        y = y.unsqueeze(1)
+        y = y.to(self.device)
+
+        if kind == 'train':
+            ds = TensorDataset(x, y)
+            if not batch_size:
+                batch_size = 1
+
+            return DataLoader(ds, batch_size=batch_size, shuffle=True)
+
+        return x, y
