024e0026de166797eb01cc4bd571ef0011fe37b1
PedroCotovio <pgcotovio@gmail.com>
2024-05-02 18:32:28 +0000

    chnage config validators

--- matcha_dl/core/entities/configs/config.py
+++ matcha_dl/core/entities/configs/config.py
@@ -1,7 +1,8 @@
 
 
-from pydantic import BaseModel, Field
-from typing import Union, Optional
+from pydantic import BaseModel, Field, field_validator
+from typing import Union, Optional, Type
+import logging
 
 from matcha_dl import config, read_yaml
 from matcha_dl.impl import models
@@ -10,34 +11,6 @@ from matcha_dl.core.contracts.model import IModel
 from matcha_dl.core.contracts.loss import ILoss
 
 import torch.optim as optim
-
-## Parsers
-
-def parse_optimizer(optimizer_name: str) -> optim.Optimizer:
-    if hasattr(optim, optimizer_name):
-        return getattr(optim, optimizer_name)
-    else:
-        raise ValueError(f"Optimizer {optimizer_name} not recognized as torch optimizer")
-
-def parse_device(device: Optional[int]):
-    if device is not None:
-        return device
-    else:
-        return 'cpu'
-
-def parse_model(model_name: str) -> IModel:
-    if hasattr(models, model_name):
-        return getattr(models, model_name)
-    else:
-        raise ValueError(f"Model {model_name} not recognized as matcha-dl model")
-
-def parse_loss(loss_name: str) -> ILoss:
-    if hasattr(losses, loss_name):
-        return getattr(losses, loss_name)
-    else:
-        raise ValueError(f"Loss {loss_name} not recognized as matcha-dl loss")
-
-## ConfigModels
 
 class MatchaParams(BaseModel):
     max_heap: str = Field(config['matcha_params']['max_heap'])
@@ -50,29 +23,61 @@ class TrainingParams(BaseModel):
     save_interval: int = Field(config['training_params']['save_interval'])
 
 class ModelParams(BaseModel):
-    model: IModel = Field(config['model']['name'], pre=[parse_model])
+    model: Type[IModel] = Field(config['model']['name'], validate_default=True)
     params: dict = Field(config['model']['params'])
 
+    @field_validator('model', mode='before')
+    def parse_model(model_name: str) -> IModel:
+        if hasattr(models, model_name):
+            return getattr(models, model_name)
+        else:
+            raise ValueError(f"Model {model_name} not recognized as matcha-dl model")
+
 class LossParams(BaseModel):
-    loss: ILoss = Field(config['loss']['name'], pre=[parse_loss])
+    loss: Type[ILoss] = Field(config['loss']['name'], validate_default=True)
     params: dict = Field(config['loss']['params'])
 
+    @field_validator('loss', mode='before')
+    def parse_loss(loss_name: str) -> ILoss:
+        if hasattr(losses, loss_name):
+            return getattr(losses, loss_name)
+        else:
+            raise ValueError(f"Loss {loss_name} not recognized as matcha-dl loss")
+
 class OptimizerParams(BaseModel):
-    optimizer: optim.Optimizer = Field(config['optimizer']['name'], pre=[parse_optimizer])
+    optimizer: Type[optim.Optimizer] = Field(config['optimizer']['name'], validate_default=True)
     params: dict = Field(config['optimizer']['params'])
+
+    @field_validator('optimizer', mode='before')
+    def parse_optimizer(optimizer_name: str) -> optim.Optimizer:
+        if hasattr(optim, optimizer_name):
+            return getattr(optim, optimizer_name)
+        else:
+            raise ValueError(f"Optimizer {optimizer_name} not recognized as torch optimizer")
 
 class ConfigModel(BaseModel):
     number_of_negatives: int = Field(config['number_of_negatives'])
     seed: int = Field(config['seed'])
-    device: Union[int, str] = Field(config['device'], pre=[parse_device])
-    logging_level: str = Field(config['logging_level'])
+    device: Union[int, str] = Field(config['device'], validate_default=True)
+    logging_level: int = Field(config['logging_level'], validate_default=True)
     use_last_checkpoint: bool = Field(config['use_last_checkpoint'])
     threshold: float = Field(config['threshold'])
     matcha_params: MatchaParams = MatchaParams()
     training_params: TrainingParams = TrainingParams()
-    model_params: ModelParams = ModelParams()
-    loss_params: LossParams = LossParams()
-    optimizer_params: OptimizerParams = OptimizerParams()
+    model: ModelParams = ModelParams()
+    loss: LossParams = LossParams()
+    optimizer: OptimizerParams = OptimizerParams()
+
+    @field_validator('logging_level', mode='before')
+    def parse_logging_level(logging_level: str) -> int:
+        return getattr(logging, logging_level.upper())
+
+    @field_validator('device', mode='before')
+    def parse_device(cls, device: Optional[int]) -> Union[int, str]:
+        if device is not None:
+            return device
+        else:
+            return 'cpu'
 
     @classmethod
     def load_config(cls, file_path: str) -> 'ConfigModel':
@@ -84,16 +89,19 @@ class ConfigModel(BaseModel):
         loss_params = LossParams(**yaml_config.get('loss', {}))
         optimizer_params = OptimizerParams(**yaml_config.get('optimizer', {}))
 
+        # filter config for set keys
+        filtered_config = {
+            k: v for k, v in yaml_config.items() 
+            if v is not None 
+            and k in cls.model_fields 
+            and k not in ['matcha_params', 'training_params', 'model', 'loss', 'optimizer']
+        }
+
         return cls(
-            number_of_negatives=yaml_config.get('number_of_negatives'),
-            seed=yaml_config.get('seed'),
-            device=yaml_config.get('device'),
-            logging_level=yaml_config.get('logging_level'),
-            use_last_checkpoint=yaml_config.get('use_last_checkpoint'),
-            threshold=yaml_config.get('threshold'),
             matcha_params=matcha_params,
             training_params=training_params,
-            model_params=model_params,
-            loss_params=loss_params,
-            optimizer_params=optimizer_params
+            model=model_params,
+            loss=loss_params,
+            optimize=optimizer_params,
+            **filtered_config
         )
