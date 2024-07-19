7dfc450c31e08c85638ed151a7f0b1fe48f14a93
PedroCotovio <pgcotovio@gmail.com>
2024-07-19 15:26:57 +0000

    refactor datasets as base and tabular, inject processor into dataset

---
+++ matcha_dl/core/entities/datasets/base.py
@@ -0,0 +1,164 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+
+from typing import Optional
+
+from mowl.owlapi import OWLAPIAdapter
+
+from matcha_dl.impl.dp.utils import read_table
+
+from jpype import java
+
+import pandas as pd
+import dgl.backend as F
+import numpy as np
+from typing import List, Tuple, Union, Dict
+
+DataFrame = pd.DataFrame
+DataArray = Union[np.ndarray, Tuple, List, F.tensor]
+
+# OWLAPI imports
+from org.semanticweb.owlapi.model import OWLOntology
+
+
+
+class Dataset(ABC):
+
+    def __init__(self, output_path: Path, matchers: List[str], **kwargs) -> None:
+
+
+        self._output_path = output_path / "dataset"
+        self._output_path.mkdir(parents=True, exist_ok=True)
+        self._matchers = matchers
+
+        self._source = None
+        self._target = None
+        self._candidates = None
+        self._reference = None
+        self._negatives = None
+        self._matcha_features = None
+
+        self._logger = kwargs.get("logger")
+        self._cache_ok = kwargs.get("cache_ok", True)
+
+    @property
+    def matchers(self) -> List[str]:
+        return self._matchers
+
+    @property
+    def source(self) -> OWLOntology:
+        return self._source
+    
+    @property
+    def target(self) -> OWLOntology:
+        return self._target
+    
+    @property
+    def candidates(self) -> DataFrame:
+        return self._candidates
+    
+    @property
+    def reference(self) -> DataFrame:
+        return self._reference
+    
+    @property
+    def negatives(self) -> DataFrame:
+        return self._negatives
+    
+    @property
+    def matcha_features(self) -> Dict[str, Dict[str, List[float]]]:
+        return self._matcha_features
+    
+    @property
+    def output_path(self) -> Path:
+        return self._output_path
+
+    
+    def load_ontologies(self, source_path: Path, target_path: Path) -> None:
+        
+        adapter = OWLAPIAdapter() 
+        owl_manager = adapter.owl_manager
+        self._source = owl_manager.loadOntologyFromOntologyDocument(
+            java.io.File(source_path))
+        
+        self.log("#Loaded Source...", level="debug")
+        
+        self._target = owl_manager.loadOntologyFromOntologyDocument(
+            java.io.File(target_path))
+        
+        self.log("#Loaded Target...", level="debug")
+
+    def load_candidates(self, file_path: Path) -> None:
+        self._candidates = read_table(str(file_path))
+        self._candidates.columns = ["Src", "Tgt", "Candidates"]
+
+        self.log("#Loaded Candidates...", level="debug")
+
+    def load_reference(self, file_path: Path) -> None:
+        self._reference = read_table(str(file_path))
+        self._reference.columns = ["Src", "Tgt", "Label"]
+
+        self.log("#Loaded Reference...", level="debug")
+
+    def load_negatives(self, file_path: str) -> None:
+        self._negatives = read_table(str(file_path))
+        self._negatives.columns = ["Src", "Tgt", "Label"]
+
+        self.log("#Loaded Negatives...", level="debug")
+
+    
+    def load_data(self, matcha_features_file: Path) -> None:
+
+        df = read_table(matcha_features_file)
+        df.columns = ["Src", "Tgt"] + self.matchers
+
+        self._matcha_features = {
+            src_ent: {
+                row["Tgt"]: row[self.matchers].values.tolist()
+                for _, row in df[df["Src"] == src_ent].iterrows()
+            }
+            for src_ent in df["Src"].unique()
+        }
+
+        self.log("#Loaded Matcha Features...", level="debug")
+
+    def log(self, msg: str, level: Optional[str] = "info"):
+        if self._logger is not None:
+            getattr(self._logger, level)(msg)
+
+        else:
+            print(msg)
+
+    @abstractmethod
+    def __getitem__(self, idx: int) -> Tuple[DataArray, DataArray]:
+        pass
+
+    @abstractmethod
+    def __len__(self) -> int:
+        pass
+
+    @abstractmethod
+    def save(self) -> Path:
+        pass
+
+    @abstractmethod
+    def load(self) -> None:
+        pass
+
+    @abstractmethod
+    def has_cache(self) -> None:
+        pass
+
+    @abstractmethod
+    def process(self) -> None:
+        pass
+
+    @abstractmethod
+    def x(self, kind: Optional[str] = "train") -> DataArray:
+        pass
+
+    @abstractmethod
+    def y(self, kind="train") -> DataArray:
+        pass
+
+
---
+++ matcha_dl/core/entities/datasets/tabular.py
@@ -0,0 +1,134 @@
+from ast import literal_eval
+from typing import Optional
+
+import numpy as np
+import pandas as pd
+
+from pathlib import Path
+
+DataFrame = pd.DataFrame
+
+from .base import Dataset
+
+from typing import Tuple
+
+
+class TabularDataset(Dataset):
+
+    def __init__(self, output_path: Path) -> None:
+        super().__init__(output_path)
+
+        self._df = None
+        self._df_save_path = self.output_path / "dataset.csv"
+
+    @property
+    def dataframe(self) -> DataFrame:
+        return self._df
+    
+    def __len__(self) -> int:
+        if self.dataframe is None:
+            return 0
+        return len(self.dataframe)
+    
+    def __getitem__(self, idx: int, kind: str = "train") -> Tuple[np.ndarray, np.ndarray]:
+
+        if idx >= len(self):
+            raise IndexError("Index out of bounds.")
+        
+        elif idx < 0:
+            raise IndexError("Index out of bounds.")
+        
+        elif len(self) == 0:
+            raise IndexError("Empty dataset.")
+        
+        return self.x(kind)[idx], self.y(kind)[idx]
+    
+    def __iter__(self):
+        for i in range(len(self)):
+            yield self[i]
+
+    def __str__(self) -> str:
+        return self.dataframe.__str__()
+
+    def x(self, kind: Optional[str] = "train") -> np.ndarray:
+        return np.array(self.dataframe[self.dataframe[kind]]["Features"].values.tolist())
+
+    def y(self, kind="train") -> np.ndarray:
+        return self.dataframe[self.dataframe[kind]]["Labels"].values
+
+    def save(self) -> Path:
+
+        self.dataframe.to_csv(str(self._df_save_path), index=False)
+
+        self.log("#Saved Dataset...", level="debug")
+
+        return self._df_save_path
+    
+    def load(self):
+        self._df = pd.read_csv(self._df_save_path, converters={"Features": literal_eval})
+
+        self.log("#Loaded Cached Dataset...", level="debug")
+
+    def has_cache(self) -> bool:
+        if self._cache_ok:
+            return self._df_save_path.exists()
+        return False
+
+    def process(self) -> None:
+
+        if self.has_cache():
+            self.load()
+            return
+        
+        if self.reference is not None:
+
+            # get training set
+            self.log("Creating Training Set...", level="debug")
+
+            # get positive samples from refs
+            positive_set = self.reference
+
+            # add negatives
+            self.log("#Adding Negative Samples...", level="debug")
+            negative_set = self.negatives
+
+            # combine positive and negative samples
+            training_set = pd.concat([positive_set, negative_set], ignore_index=True)
+
+            # get scores features from matcha
+            self.log("#Getting Scores...", level="debug")
+            training_set = self._get_scores(training_set)
+
+            # assign training label
+            training_set["train"] = True
+            training_set["inference"] = False
+
+            self.log("#Shuffling Training Set...", level="debug")
+
+            training_set = training_set.sample(frac=1).reset_index(drop=True)
+
+            
+
+    def _get_matcha_features(self, dataset: pd.DataFrame) -> pd.DataFrame:
+
+        feats = []
+        flag = 0
+
+        for _, row in dataset.iterrows():
+
+            try:
+                scores = self.scores.get(row["Src"]).get(row["Tgt"])
+            except AttributeError:
+                raise ValueError("Scores for source {} and target {} not found.".format(row["Src"], row["Tgt"]))
+            
+            feats.append(scores)
+            
+
+        dataset["Features"] = feats
+
+        return dataset
+
+            
+            
+
+
