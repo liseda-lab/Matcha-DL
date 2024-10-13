af53611e9c096aac22be3108c0d9de84836176c6
pedrocotovio <pgcotovio@gmail.com>
2024-10-13 04:49:19 +0000

    refactor matcha comunication to wait for matcha process before proceding

--- matcha_dl/core/contracts/matcha.py
+++ matcha_dl/core/contracts/matcha.py
@@ -4,7 +4,14 @@ import subprocess
 import sys
 from abc import abstractmethod
 from pathlib import Path
-from typing import Optional, Tuple, List
+from typing import Optional, List
+import queue
+import threading
+import select
+import time
+import pandas as pd
+from ast import literal_eval
+from matcha_dl.impl.dp.utils import read_table
 
 MATCHA = "matcha"
 
@@ -40,7 +47,7 @@ class IMatcha:
         self._candidates = self.output_path / "candidates.tsv"
         self._negatives = self.output_path / "negatives.tsv"
         self._matcha_features = self.output_path / "matcha_features.tsv"
-        self._log_file = self.output_path / "matcha.log"
+        self._log_file = self.output_path / "matcha_error.log"
 
         self._logger = kwargs.get("logger")
         self._cache_ok = kwargs.get("cache_ok", True)
@@ -113,6 +120,10 @@ class IMatcha:
         if self._cache_ok:
             return self.matcha_features.exists()
         return False
+
+    @property
+    def max_heap(self) -> str:
+        return self._max_heap
     
     def load_ontologies(self, source_path: Path, target_path: Path) -> None:
 
@@ -127,34 +138,103 @@ class IMatcha:
         self.log("#Loaded Reference Path...", level="debug")
 
     def load_candidates(self, file_path: Path) -> None:
-        self._candidates = file_path
+
+        def get_cands(df: pd.DataFrame) -> pd.DataFrame:
+
+            return pd.DataFrame([
+                    [source, cand, None]
+                    for source, _, target_cands in df.values
+                    for cand in literal_eval(target_cands)
+                ], columns=["Src", "Tgt", "Score"])
+
+        # Load One2Many candidates file
+        candidates = read_table(str(file_path))
+        candidates.columns = ["Src", "Tgt", "Candidates"]
+
+        # Get One2One candidates df
+        candidates = get_cands(candidates)
+
+        # Save One2One candidates
+        candidates.to_csv(self.candidates, sep="\t", index=False)
 
         self.log("#Loaded Candidates Path...", level="debug")
 
     def match(self) -> None:
 
-        def comunicate_matcha_process(matcha_process, input):
+        def read_output(process, output_queue, stop_event):
+            while not stop_event.is_set():
+                # Use select to wait for the process's stdout to be ready for reading
+                ready, _, _ = select.select([process.stdout], [], [], 1.0)
+                if ready:
+                    line = process.stdout.readline().strip()
+                    if line:
+                        output_queue.put(line)
+                    else:
+                        break
+
+        def wait_for_reply(matcha_process, termination):
+
+            # Create a queue to hold the output
+            output_queue = queue.Queue()
+
+            # Create an Event object to signal the thread to stop
+            stop_event = threading.Event()
+
+            # Start a thread to read the output
+            output_thread = threading.Thread(target=read_output, args=(matcha_process, output_queue, stop_event))
+            output_thread.start()
+
+            # Wait for a response from the process
+            while matcha_process.poll() is None:
+                try:
+                    # Get a line of output from the queue
+                    line = output_queue.get(timeout=1)
+                    self.log(f"[Matcha] {line}", level="debug")
+                    # Check for a specific response indicating the command is done
+                    if termination in line:
+                        break
+                except queue.Empty:
+                    # No output received within the timeout period
+                    continue
+
+            # Signal the output thread to stop
+            stop_event.set()
+
+            # Ensure the output thread is finished
+            output_thread.join()
+
+            if matcha_process.poll() == 0:
+                self.log(f"Matcha process finished without error code unespectedly", level="debug")
+            elif matcha_process.poll() is not None:
+                raise RuntimeError(f"Matcha subprocess returned with error code {matcha_process.returncode} check error log at {self.log_file}")
+
+        def comunicate_matcha_process(matcha_process, input, termination=None):
             self.log(f"Running command in Matcha: {input}", level="debug")
-            stdout, stderr = matcha_process.communicate(input=input)
-            self.log_file.write(stdout)
-            self.log_file.write(stderr)
+            matcha_process.stdin.write(input + "\n")
+            matcha_process.stdin.flush()
 
-        def add_matchers(matcha_process, log_file):
+            if termination is None:
+                time.sleep(1)
+                return
+
+            wait_for_reply(matcha_process, termination)
+
+        
+        def add_matchers(matcha_process):
             command = f"Matchers {{{', '.join(self.matchers)}}}"
-            comunicate_matcha_process(matcha_process, command, log_file)
+            comunicate_matcha_process(matcha_process, command)
 
-        def generate_negatives(matcha_process, log_file):
-            command = f"negatives {self.reference} {self.negatives}"
-            comunicate_matcha_process(matcha_process, command, log_file)
+        def generate_negatives(matcha_process):
+            command = f"Negatives {self.reference} {self.negatives}"
+            comunicate_matcha_process(matcha_process, command, 'Finished generating negatives')
 
-        def generate_candidates(matcha_process, log_file):
+        def generate_candidates(matcha_process):
             command = f"Match {self.threshold} {self.candidates}"
-            comunicate_matcha_process(matcha_process, command, log_file)
+            comunicate_matcha_process(matcha_process, command, 'Finished matching')
 
-        def generate_scores(matcha_process, log_file, pairs_file):
+        def generate_scores(matcha_process, pairs_file):
             command = f"Score {pairs_file} {self.matcha_features}"
-            comunicate_matcha_process(matcha_process, command, log_file)
-
+            comunicate_matcha_process(matcha_process, command, 'Finished calculating scores')
 
         if self.has_cache:
 
@@ -164,6 +244,9 @@ class IMatcha:
             )
 
             return
+
+        if self.source is None or self.target is None:
+            raise FileNotFoundError("Ontologies not loaded")
         
         # Main Matcha Execution
 
@@ -175,33 +258,44 @@ class IMatcha:
             "-jar",
             f"-Xmx{self.max_heap}",
             str(self.jar_path),
-            str(self.source),
-            str(self.target),
-            sys.executable,
+            '-s', str(self.source),
+            '-t', str(self.target),
+            '-p', sys.executable,
         ]
 
         try:
-            with open(str(self.log_file), "w") as f:
 
-                # Load Matcha jar file with ontologies
+            # Load Matcha jar file with ontologies
 
-                self.log(f"Running Matcha with command: {jar_command}", level="debug")
+            self.log(f"Running Matcha with command: {jar_command}", level="debug")
 
-                matcha_process = subprocess.Popen(jar_command, stdin=subprocess.PIPE, stdout=f, stderr=f, text=True)
+            with open(self.log_file, "w") as f:
+
+                matcha_process = subprocess.Popen(jar_command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=f, text=True)
+
+                wait_for_reply(matcha_process, 'Matcha CLI activated')
 
                 # Load matchers to use
 
-                add_matchers(matcha_process, f)
+                add_matchers(matcha_process)
 
                 # If candidates exist, skip, otherwise get candidates with matcha Match command
 
                 if not self.candidates.exists():
-                    generate_candidates(matcha_process, f)
+                    generate_candidates(matcha_process)
+
+                    if not self.candidates.exists():
+                        raise FileNotFoundError(f"Matcha failed to generate candidates at {self.candidates}")
+
+                else:
+                    self.log(f"Matcha candidates already exist at {self.candidates}. Skipping computation.", level="info")
 
                 # If reference exists, get negatives from reference, otherwirse skip
 
                 if self.reference is not None and self.reference.exists():
-                    generate_negatives(matcha_process, f)
+                    generate_negatives(matcha_process)
+                    if not self.negatives.exists():
+                        raise FileNotFoundError(f"Matcha failed to generate negatives at {self.negatives}")
 
                 # Compile all files to generate scores into a single file (reference/negatives/candidates)
 
@@ -212,18 +306,29 @@ class IMatcha:
                         pairs.write(candidates.read())
                     if self.reference is not None and self.reference.exists():
                         with open(self.reference, "r") as reference:
+                            next(reference)  # Skip header
                             pairs.write(reference.read())
                         with open(self.negatives, "r") as negatives:
+                            next(negatives)  # Skip header
                             pairs.write(negatives.read())
 
                 # Use Scores command to get scores from matcha
 
-                generate_scores(matcha_process, f, pairs_file)
+                generate_scores(matcha_process, pairs_file)
+
+                if not self.matcha_features.exists():
+                    raise FileNotFoundError(f"Matcha failed to generate matcha features at {self.matcha_features}")
 
         except subprocess.CalledProcessError as e:
             raise RuntimeError(f"Matcha subprocess returned with error code {e.returncode}")
-        
-        os.chdir(current_cwd)
+
+        finally:
+
+            # stop matcha process
+            matcha_process.terminate()
+
+            # change back to original directory
+            os.chdir(current_cwd)
 
         self.log(f"Matcha scores written to {self.matcha_features}", level="info")
 
